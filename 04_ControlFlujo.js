a26575d795aac456241b7acc0adf1a39489d15d4
lucialeitessanchez <lucialeitessanchez@gmail.com>
2021-11-03 12:55:35 -0300

    control de flujo ifandelse

--- 04_ControlFlujo.js
+++ 04_ControlFlujo.js
@@ -13,3 +13,37 @@ var administrador = 'administrador';
 if (administrador === 'administrador') {
     console.log('Bienvenido al sistema');
 }
+
+const MAYORIA_DE_EDAD = 18;
+var edadPErsona = 18;
+
+
+if (edadPErsona >= MAYORIA_DE_EDAD) {
+    console.log('Es mayor de edad');
+} else {
+    console.log('Es menor de edad ');
+}
+
+//ejemplo semaforo de carrera
+
+var semaforo = 'purpura';
+if (semaforo === 'verde') {
+    console.log('Arranca');
+}else if(semaforo === 'amarillo'){
+    console.log('Listo?');
+}else if(semaforo === 'rojo'){
+    console.log('motores apagados');
+} else {                                // si ninguna condicion de las anteriores se cumple entonces ejecuta esta 
+    console.log('color no identificado');
+}
+
+//switch
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
