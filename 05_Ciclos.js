837815f58f6bb50170370c68576763a829f23e32
lucialeitessanchez <lucialeitessanchez@gmail.com>
2021-11-04 19:55:11 -0300

    empece con ciclos

---
+++ 05_Ciclos.js
@@ -0,0 +1,23 @@
+/**
+ * Ciclos
+ */
+
+//while
+var contador = 0;
+while(contador <= 100){
+    console.log('Hola mundo!');
+    contador++;
+}
+
+//do-while 
+var contador =1;
+do {
+    console.log('Hola mundo!');
+    contador +=1;
+} while (contador <= 10);
+
+//for iterar un elemento o repetir una serie de elementos
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
