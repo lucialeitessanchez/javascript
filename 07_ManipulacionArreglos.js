9ef1aea5ea4040a347c726428d9d4e3b38e0292b
lucialeitessanchez <lucialeitessanchez@gmail.com>
2021-11-17 12:04:15 -0300

    funciones mutables

--- 07_ManipulacionArreglos.js
+++ 07_ManipulacionArreglos.js
@@ -18,3 +18,22 @@ console.log(letras.length);
 
 letras.forEach((element) => console.log(element));
 
+//push shift pop, 3 funciones
+
+var letras =['a','b','c','d','e'];
+letras.push('f');
+console.log(letras);
+
+//shift nos sirve para obetener el primer elemento de algo, va a modificar el arreglo, lo va a dejar sin el primer elemento digamos
+var primerElemento =letras.shift();
+console.log(primerElemento); //sacamos el primer elemento pero lo guardamos
+console.log(letras);
+letras.pop(); //lo mismo que shift pero con el ultimo elemento
+console.log(letras);
+
+
+//map ejemplo de asistencia 
+var estudiantes = ['Dayana','Pamela','Ada','German'];
+var asistencia = estudiantes.map(()=>{
+    return 
+});
