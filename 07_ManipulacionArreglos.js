64421d495d4d15941c3f177b93b32d89aed3be8f
lucialeitessanchez <lucialeitessanchez@gmail.com>
2021-11-18 15:53:40 -0300

    map y filter

--- 07_ManipulacionArreglos.js
+++ 07_ManipulacionArreglos.js
@@ -33,7 +33,52 @@ console.log(letras);
 
 
 //map ejemplo de asistencia 
-var estudiantes = ['Dayana','Pamela','Ada','German'];
-var asistencia = estudiantes.map(()=>{
-    return 
-});
+var estudiantes = ['Dayana','Pamela','Ada','German']; // vamos a transformar cada uno de estos elementos en objetos pero no va a mutar 
+var asistencia = estudiantes.map((nombre)=> `${nombre}.`)
+/*var asistencia = estudiantes.map((nombre)=>{
+    return  {
+        nombre: nombre,
+        assistencia: false
+    }
+});*/
+
+console.log(estudiantes);
+console.log(asistencia);
+console.log(estudiantes);
+
+var productos = [
+    {nombre: 'camisetas',precio: 15},
+    {nombre: 'zapatillas',precio: 20},
+    {nombre: 'pantalon',precio: 25}
+]
+
+var productosImpuesto = productos.map((producto)=>{
+    return {
+        ...producto,
+        impuesto: .12
+    }
+})
+
+var precios = productos.map((producto)=> producto.precio) // por cada iteracion inyecta el precio (osea lo saca ) y ya con la funcion => tiene implicito el return
+
+console.log(productos); 
+console.log(productosImpuesto);
+console.log(precios);
+
+//filter inmutable, no va a modificar el arreglo de orgien, va a ayudarnos a filtrar el elemento de un objeto en base a una condicion que debe devolver un valor logico
+var estudiantes = [
+    {nombre: 'Ada',edad:20,matriculado:true},
+    {nombre: 'Lucia',edad:20,matriculado:true},
+    {nombre: 'Rodrigo',edad:19,matriculado:false},
+    {nombre: 'Maria',edad:22,matriculado:false},
+    {nombre: 'Leo',edad:21,matriculado:true},
+    {nombre: 'Raul',edad:19,matriculado:false}
+]
+
+//var filtrado = estudiantes.filter((estudiante)=> estudiante.edad >= 21);
+var filtrado = estudiantes.filter((estudiante)=> estudiante.edad >= 21 && estudiante.matriculado);
+console.log(filtrado);
+console.log(estudiantes);
+
+//reduce reduce el todo el arreglo a un solo valor, recibd dos parametros, el primero es una funcion 
+ 
